684ffa322e6e522776f742c8f3371a561cc5acf8
Vicmart <vickyd@Vickys-MacBook-Pro.local>
2015-05-07 17:56:20 -0400

    fixed jquery ish

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+    $('#the_header').addClass('animated infinite bounce');
+});
 $('#left_button').click(function() {
 	$('#the_header').addClass('animated infinite fadeOut');
 	alert( "Handler for .click() called." );
