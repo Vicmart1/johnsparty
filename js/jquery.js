7c17d32fedf6b4a09b34dffbee7eaea4cd3f2711
Vicmart <vickyd@Vickys-MacBook-Pro.local>
2015-05-07 18:24:19 -0400

    added animations

--- js/jquery.js
+++ js/jquery.js
@@ -5,14 +5,24 @@ $("input").click(function(e){
     var idClicked = e.target.id;
     //alert("You clicked" + idClicked);
 });
+
+var sayThankYou = function () {
+    $('#the_body').html('Thank you for your input.');
+    $('#the_body').removeClass('animated fadeOut');
+    $('#the_body').addClass('animated fadeIn');
+}
+
+
 $('#left_button').click(function() {
                         $('#the_header').addClass('animated fadeOut');
                         $('#the_body').addClass('animated fadeOut');
-                        //alert( "Handler for .click() called." );
+                        $('#the_body').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', sayThankYou());
 });
+
 
 $('#right_button').click(function() {
                         $('#the_header').addClass('animated fadeOut');
                         $('#the_body').addClass('animated fadeOut');
+                        $('#the_body').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', sayThankYou());
                         //alert( "Handler for .click() called." );
 });
