702abf0119612c4814938c402759d60bcd8db35b
Vicmart <vickyd@Vickys-MacBook-Pro.local>
2015-05-07 17:54:45 -0400

    fixed jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,4 @@
 $('#left_button').click(function() {
-	$('#the_header').addClass('animated fadeOut');
+	$('#the_header').addClass('animated infinite fadeOut');
 	alert( "Handler for .click() called." );
 });
