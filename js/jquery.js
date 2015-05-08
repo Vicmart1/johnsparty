4c517628c8dfe400184ce62bb43afdb820a9f42b
Vicmart <vickyd@Vickys-MacBook-Pro.local>
2015-05-08 02:05:50 -0400

    fixed some bugs

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
-                  Parse.initialize("88lDbflAf4cF2dTqDwFHkv6N6EG6WtDXY0YVT2lR", "YIPG0FJNlDrGcG5EaKdXHe74xQNGASrem08Pr6to");
-});
+var questions = 0;
+var left_buttons = new Array;
+var right_buttons = new Array;
+
+var createElement = function (element, where) {
+    $( element ).appendTo( where );
+}
+
 $("input").click(function(e){
     var idClicked = e.target.id;
 });
@@ -8,10 +13,10 @@ $("input").click(function(e){
 var sayThankYou = function (result) {
     $('#the_body').removeClass('animated fadeOut');
     $('#the_body').addClass('animated fadeIn');
-    $('#right_button').remove();
-    $('#left_button').html('Refresh Page');
+    $('#right_button1').remove();
+    $('#left_button1').html('Refresh Page');
     
-    jQuery('#left_button').unbind('click');
+    jQuery('#left_button1').unbind('click');
     var TestObject = Parse.Object.extend("Result");
     var testObject = new TestObject();
     testObject.save({score: result}).then(function(object) { });
@@ -22,6 +27,7 @@ var sayThankYou = function (result) {
     var ctx = $("#myChart").get(0).getContext("2d");
     // This will get the first returned node in the jQuery collection.
     var myNewChart = new Chart(ctx);
+    
     
     var GameScore = Parse.Object.extend("Result");
     var query = new Parse.Query(GameScore);
@@ -67,28 +73,38 @@ var sayThankYou = function (result) {
                
                },
                error: function(error) {
-               alert("Error: " + error.code + " " + error.message);
+               //alert("Errorrr: " + error.code + " " + error.message);
                }
                });
     
     
-    jQuery('#left_button').click(function(){
+        $(document).on('click','#left_button1', function(){
                         location.reload();
                         });
 }
 
-$('#left_button').click(function() {
+$(document).on('click','#left_button1' , function() {
                         var temp = "1";
                         $('#the_header').addClass('animated fadeOut');
                         $('#the_body').addClass('animated fadeOut');
                         $('#the_body').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', sayThankYou(temp));
 });
 
-
-$('#right_button').click(function() {
+$(document).on('click','#right_button1' , function() {
                         var temp = "2";
                         $('#the_header').addClass('animated fadeOut');
                         $('#the_body').addClass('animated fadeOut');
                         $('#the_body').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', sayThankYou(temp));
                         //alert( "Handler for .click() called." );
+});
+
+$(document).ready(function() {
+                  Parse.initialize("88lDbflAf4cF2dTqDwFHkv6N6EG6WtDXY0YVT2lR", "YIPG0FJNlDrGcG5EaKdXHe74xQNGASrem08Pr6to");
+                  questions++;
+                  createElement("<p class='lead' id='question1'>Are you a left shark or a right shark?</p>","#cover");
+                  createElement("<p class='lead' id='container"+questions+"'></p>","#cover");
+                  left_buttons.push("<a href='#' class='btn btn-lg btn-default left' id='left_button1'>Left Shark</a>");
+                  right_buttons.push("<a href='#' class='btn btn-lg btn-default right' id='right_button1'>Right Shark</a>");
+                  createElement(left_buttons[0],"#container"+questions+"");
+                  createElement(right_buttons[0],"#container"+questions+"");
 });
