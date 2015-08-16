cffbdf6f8350c9968522edcf744336a50d6f57db
Vicmart1 <vdadfar1@gmail.com>
2015-08-16 19:57:39 -0400

    reworking site

--- js/jquery.js
+++ js/jquery.js
@@ -6,30 +6,45 @@ var createElement = function (element, where) {
     $( element ).appendTo( where );
 }
 
+var createButton = function(element, where, position, num) {
+	createElement(element, where);
+	var position_string;
+	if(position == 1)
+		position_string = "left";
+	else
+		position_string = "right";	
+	$(document).on('click','#' + position_string + '_button' + num , function() {
+                        $('#the_header').addClass('animated fadeOut');
+                        $('#the_body').addClass('animated fadeOut');
+                        $('#the_body').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', sayThankYou(position,num));
+	});
+}
+
 $("input").click(function(e){
     var idClicked = e.target.id;
 });
 
-var sayThankYou = function (result) {
+var sayThankYou = function (result, num) {
     $('#the_body').removeClass('animated fadeOut');
     $('#the_body').addClass('animated fadeIn');
-    $('#right_button1').remove();
-    $('#left_button1').html('Refresh Page');
-    
-    jQuery('#left_button1').unbind('click');
-    var TestObject = Parse.Object.extend("Result");
+    $('#right_button'+num).remove();
+	$('#left_button'+num).remove();
+    //$('#left_button'+num).html('Refresh Page');
+
+	
+    var TestObject = Parse.Object.extend("Result"+num);
     var testObject = new TestObject();
-    testObject.save({score: result}).then(function(object) { });
+    testObject.save({score: String(result)}).then(function(object) { });
     
-    $( "<canvas id='myChart' width='400' height='400'></canvas>" ).appendTo( "#cover" );
+    $( "<canvas id='myChart"+num+"' width='400' height='400'></canvas>" ).appendTo( "#question"+num );
     
     // Get context with jQuery - using jQuery's .get() method.
-    var ctx = $("#myChart").get(0).getContext("2d");
+    var ctx = $("#myChart"+num).get(0).getContext("2d");
     // This will get the first returned node in the jQuery collection.
     var myNewChart = new Chart(ctx);
     
     
-    var GameScore = Parse.Object.extend("Result");
+    var GameScore = Parse.Object.extend("Result"+num);
     var query = new Parse.Query(GameScore);
     query.limit(1000);
     //query.equalTo("playerName", "Dan Stemkoski");
@@ -77,34 +92,24 @@ var sayThankYou = function (result) {
                }
                });
     
-    
-        $(document).on('click','#left_button1', function(){
-                        location.reload();
-                        });
+    	
+        //$(document).on('click','#left_button'+num, function(){
+        //               location.reload();
+        //                });
 }
 
-$(document).on('click','#left_button1' , function() {
-                        var temp = "1";
-                        $('#the_header').addClass('animated fadeOut');
-                        $('#the_body').addClass('animated fadeOut');
-                        $('#the_body').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', sayThankYou(temp));
-});
+var createPrompt = function(question, answer1, answer2) {
+                  questions++;
+                  createElement("<p class='lead' id='question"+questions+"'>"+question+"</p>","#cover");
+                  createElement("<p class='lead' id='container"+questions+"'></p>","#cover");
+                  left_buttons.push("<a href='#' class='btn btn-lg btn-default left' id='left_button"+questions+"'>"+answer1+"</a>");
+                  right_buttons.push("<a href='#' class='btn btn-lg btn-default right' id='right_button"+questions+"'>"+answer2+"</a>");
+                  createButton(left_buttons[questions-1],"#container"+questions, 1, questions);
+                  createButton(right_buttons[questions-1],"#container"+questions, 2, questions);
 
-$(document).on('click','#right_button1' , function() {
-                        var temp = "2";
-                        $('#the_header').addClass('animated fadeOut');
-                        $('#the_body').addClass('animated fadeOut');
-                        $('#the_body').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', sayThankYou(temp));
-                        //alert( "Handler for .click() called." );
-});
+}
 
 $(document).ready(function() {
                   Parse.initialize("88lDbflAf4cF2dTqDwFHkv6N6EG6WtDXY0YVT2lR", "YIPG0FJNlDrGcG5EaKdXHe74xQNGASrem08Pr6to");
-                  questions++;
-                  createElement("<p class='lead' id='question1'>Are you a left shark or a right shark?</p>","#cover");
-                  createElement("<p class='lead' id='container"+questions+"'></p>","#cover");
-                  left_buttons.push("<a href='#' class='btn btn-lg btn-default left' id='left_button1'>Left Shark</a>");
-                  right_buttons.push("<a href='#' class='btn btn-lg btn-default right' id='right_button1'>Right Shark</a>");
-                  createElement(left_buttons[0],"#container"+questions+"");
-                  createElement(right_buttons[0],"#container"+questions+"");
+                  createPrompt("Are you a left shark or right shark?", "Left Shark", "Right Shark");
 });
